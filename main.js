62ff7a08e5498c1c600a5d8514a1080fe63d5412
miliberlin <michelle.liebheit@posteo.de>
2021-08-26 15:15:33 +0200

    Practice binding attributes

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
+      description: "These are some awesome socks!",
+      image: "./assets/images/socks_green.jpg",
+      url: "http://test.de",
     };
   },
 });
