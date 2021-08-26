8a4ce364e7fee51482b97f656355cf1ee59dbc6c
miliberlin <michelle.liebheit@posteo.de>
2021-08-26 16:09:34 +0200

    Handle click events

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: "Socks",
       description: "These are some awesome socks!",
       image: "./assets/images/socks_green.jpg",
@@ -9,10 +10,23 @@ const app = Vue.createApp({
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
       sizes: ["36-38", "39-41", "42-44"],
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateImage(variantImg) {
+      this.image = variantImg;
+    },
   },
 });
