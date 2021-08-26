57a1faba303ee6d32b30719e90207e431280403b
miliberlin <michelle.liebheit@posteo.de>
2021-08-26 15:58:55 +0200

    Practice list rendering with v-for

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
       url: "http://test.de",
       inventory: 8,
       onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
+      sizes: ["36-38", "39-41", "42-44"],
     };
   },
 });
